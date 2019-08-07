62148067a72347454441f6eb9b594041dc0089d1
h-sawai <h-sawai@quartetcom.co.jp>
2019-08-07 14:03:54 +0900

    ccに対応するためにスクリプトを修正

--- samples/js/script.js
+++ samples/js/script.js
@@ -1,9 +1,21 @@
 $(function () {
-    var items = [];
+    var items = {};
     $('.form-group').each(function () {
-        items.push($(this).find('div,pre').text());
+        var key = $(this).find('label').text().toLowerCase();
+        if (key === 'subject') {
+            key = 'su';
+        }
+        items[key] = encodeURIComponent($( this ).find( 'div,pre' ).text());
     });
 
-    var result = 'https://mail.google.com/mail/?view=cm&bcc=' + items[0] + '&su=' + encodeURIComponent(items[1]) + '&body=' + encodeURIComponent(items[2]);
+    var result = 'https://mail.google.com/mail/?view=cm';
+    for ( var key in items ) {
+        if ( items.hasOwnProperty( key ) ) {
+            if ( key === 'subject' ) {
+                key = 'su';
+            }
+            result += '&' + key + '=' + items[key];
+        }
+    }
     $('#link').attr('href', result);
 });
