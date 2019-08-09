42c4858d492a21eab838e5b117c08d83e3d1090d
Takashi Kanemoto <ttskch@gmail.com>
2019-08-09 13:38:11 +0900

    コーディングスタイルを修正＆不要なコードを削除

--- samples/js/script.js
+++ samples/js/script.js
@@ -5,17 +5,12 @@ $(function () {
         if (key === 'subject') {
             key = 'su';
         }
-        items[key] = encodeURIComponent($( this ).find( 'div,pre' ).text());
+        items[key] = encodeURIComponent($(this).find('div,pre').text());
     });
 
     var result = 'https://mail.google.com/mail/?view=cm';
-    for ( var key in items ) {
-        if ( items.hasOwnProperty( key ) ) {
-            if ( key === 'subject' ) {
-                key = 'su';
-            }
-            result += '&' + key + '=' + items[key];
-        }
+    for (var key in items) {
+        result += '&' + key + '=' + items[key];
     }
     $('#link').attr('href', result);
 });
